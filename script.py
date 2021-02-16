f25422b0ac8d5602680964731efbb73edc60406a
Leo <leomartinsds@gmail.com>
2021-02-16 15:31:33 -0300

    subtitles

--- script.py
+++ script.py
@@ -1,4 +1,16 @@
-import requests
+import re
+bad_words = ['-->']
 
-r = requests.get("https://coreyms.com")
-print(r.status_code)
+
+with open('MichaelPollan.vtt') as oldfile, open('newfile.txt', 'w') as newfile:
+    for line in oldfile:
+        if not any(bad_word in line for bad_word in bad_words):
+            newfile.write(line)
+
+
+with open('newfile.txt') as result:
+    uniqlines = set(result.readlines())
+    with open('sub_out.txt', 'w') as rmdup:
+        mylst = map(lambda each: each.strip("&gt;&gt;"), uniqlines)
+        print(mylst)
+        rmdup.writelines(set(mylst))
